c4d1264b56d2cbaa81a82038eef02d10b4a32b7c
QbProg <tholag@gmail.com>
2015-09-17 09:38:30 +0200

    Fixes for static library compilation

--- Depends.cmake
+++ Depends.cmake
@@ -3,6 +3,9 @@ message("Setting oce-win-bundle depends...")
 # Add dependencies between OCE and oce-win-bundle
 if(OCE_WITH_FREEIMAGE)
     add_dependencies( TKService FreeImage )
+    if(NOT BUNDLE_SHARED_LIBRARIES)
+		TARGET_COMPILE_DEFINITIONS(TKService PRIVATE "-DFREEIMAGE_LIB")
+	endif()
 endif()
 
 if (OCE_VISUALISATION)
