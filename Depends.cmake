6282670f8d07194d8d91a0cdd42552ba49e61184
QbProg <tholag@gmail.com>
2015-12-29 21:56:32 +0100

    Small fix

--- Depends.cmake
+++ Depends.cmake
@@ -2,10 +2,10 @@ message("Setting oce-win-bundle depends...")
 
 # Add dependencies between OCE and oce-win-bundle
 if(OCE_WITH_FREEIMAGE)
-    add_dependencies( TKService FreeImage )
-    if(NOT BUNDLE_SHARED_LIBRARIES)
-		TARGET_COMPILE_DEFINITIONS(TKService PRIVATE "-DFREEIMAGE_LIB")
-	endif()
+    add_dependencies( TKService FreeImage FreeImagePlus )
+    if(NOT OCE_BUILD_SHARED_LIB)
+		   TARGET_COMPILE_DEFINITIONS(TKService PRIVATE "-DFREEIMAGE_LIB")
+	  endif()
 endif()
 
 if (OCE_VISUALISATION)
